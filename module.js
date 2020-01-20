a965fac9a8feed7d8fbd287526475ae47432d594
Willy <wsquarepa@gmail.com>
2020-01-19 19:46:37 -0800

    uncommented the connection part

--- module.js
+++ module.js
@@ -5,22 +5,22 @@ var mysql = require('mysql');
 const fs = require('fs');
 //var index = require('./index.html')
 
-// var con = mysql.createConnection({
-//   host: "104.248.218.189",
-//   port:"22",
-//   user: "webaccess",
-//   password: "amazingWeb",
-//   database:"paauctionsite"
-// });
+var con = mysql.createConnection({
+  host: "localhost",
+  port:"22",
+  user: "webaccess",
+  password: "amazingWeb",
+  database:"paauctionsite"
+});
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   // con.query("", function (err, result) {
-//   //   if (err) throw err;
-//   //   console.log("Database created");
-//   // });
-// });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  // con.query("", function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Database created");
+  // });
+});
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
