87e5dba5b2e17a51c11b54ab9861e773e813aea7
Willy <wsquarepa@gmail.com>
2020-05-09 18:57:58 -0700

    set up firebase hosting and fixed functions

--- functions/index.js
+++ functions/index.js
@@ -17,42 +17,92 @@ let transporter = nodemailer.createTransport({
 });
 
 exports.auctionCheck = functions.https.onRequest((req, res) => {
-    database.ref("/auctionItems").once('value').then(function(snapshot) {
-        var values = snapshot.val()
-        var keys = Object.keys(values)
-        var now = new Date()
-        for (var i = 0; i < keys.length; i++) {
-            if (Date(values[keys[i]]["expires"]) <= now) {
-                var value = values[keys[i]]
-                sendMail(value["emailOfSeller"], "auction-ended", keys[i], value["currentBidder"], value["itemPrice"])
-                database.ref("auctionItems/" + keys[i]).remove()
+    console.log("Opening database...")
+    admin.database().ref("/auctionItems").once('value', function(snapshot) {
+        console.log("Starting 'for' loop.")
+        var values = snapshot.val() || "No value"
+        if (values != "No value") {
+            var keys = Object.keys(values)
+            var now = new Date().getTime()
+            for (var i = 0; i < keys.length; i++) {
+                console.log("Loop time: " + i)
+                var endTime = new Date(parseInt(values[keys[i]]["expires"])).getTime()
+                console.log(endTime)
+                console.log(now)
+                if (now > endTime) {
+                    console.log("It's email time!")
+                    var value = values[keys[i]]
+                    //sendMail(value["emailOfSeller"], "auction-ended", keys[i], value["currentBidder"], value["itemPrice"])
+                    if (value["currentBidder"] != "") {
+                        var mailOptions = {}
+                        mailOptions = {
+                            from: 'Pacific Academy Auction Site <paauctionsite@gmail.com>',
+                            to: value["emailOfSeller"],
+                            subject: "Your auction ended", // email subject
+                            html: `<h1> Your aucitoned item ended. </h1> 
+                            <p style="font-size:14">Your auction item, ${keys[i]}, had expired on the store. <br>
+                            The winner of the auciton is <a href='mailto:${value["currentBidder"]}'>${value["currentBidder"]}</a> and he/she is willing to pay
+                            $${value["itemPrice"]} for your item.</p>` // email content in HTML
+                        };
+
+                        var mailOptions2 = {}
+                        mailOptions2 = {
+                            from: 'Pacific Academy Auction Site <paauctionsite@gmail.com>',
+                            to: value["currentBidder"],
+                            subject: "You won an auction!", // email subject
+                            html: `<h1> You've won an item! </h1> 
+                            <p style="font-size:14">The auction item, ${keys[i]}, had expired on the store. <br>
+                            You have won the item and the final price is $${value["itemPrice"]} for the item. <br>
+                            Please get in contact with <a href='mailto:${value["emailOfSeller"]}'>${value["emailOfSeller"]}</a> soon to arrange the item transfer!</p>` // email content in HTML
+                        }
+
+                        transporter.sendMail(mailOptions)
+                        transporter.sendMail(mailOptions2)
+                        
+                    } else {
+                        var mailOptions = {}
+                        mailOptions = {
+                            from: 'Pacific Academy Auction Site <paauctionsite@gmail.com>',
+                            to: value["emailOfSeller"],
+                            subject: "Your auction ended", // email subject
+                            html: `<h1> Your aucitoned item ended. </h1> 
+                            <p style="font-size:14">Your auction item, ${keys[i]}, had expired on the store. <br>
+                            Unfortunatley, nobody wanted your item. Try pricing it lower, or make a new one!</p>` // email content in HTML
+                        };
+
+                        transporter.sendMail(mailOptions)
+                    }
+                    database.ref("auctionItems/" + keys[i]).remove()
+                }
+                console.log("Loop " + i + " complete, starting next loop.")
             }
+            console.log("Successful looping.")
+            res.status(200).send("Execution complete.")
+        } else {
+            res.status(200).send("No execution to do.")
         }
+        
     })
 });
 
 exports.accountCreated = functions.https.onRequest((req, res) => {
     cors(req, res, function() {
         var dest = req.query.dest;
-        var password = req.query.password
         mailOptions = {
             from: 'Pacific Academy Auction Site <paauctionsite@gmail.com>',
             to: dest,
             subject: "Thank you for creating an account!", // email subject
             html: `<head>
                         <style>
-                            .passwordField {
-                                background-color: #000000
-                            }
-                            
-                            .passwordField:hover {
-                                background-color: #ffffff
+                            body {
+                                font-size: 14px;
                             }
                         </style>
                     </head>
                     <body>
-                            <h1>Thank you for creating an account!</h1>
-                            The password to your account, ${dest}, is <div class="passwordField">${password}</div>
+                            <h1>Welcome to the Pacific academy Auction site!</h1>
+                            Thank you for creating an account, ${dest}! I hope you have fun selling items!
+                            -William
                     </body>` // email content in HTML
         };
 
@@ -62,12 +112,6 @@ exports.accountCreated = functions.https.onRequest((req, res) => {
             }
             return res.send('<script>javascript:history.go(-1)</script>');
         });
-    })
-})
-
-exports.functionsTest = functions.https.onRequest((req, res) => {
-    cors(req, res, function() {
-        res.send("<h1>Error 200 not forbidden :0</h1>")
     })
 })
 
